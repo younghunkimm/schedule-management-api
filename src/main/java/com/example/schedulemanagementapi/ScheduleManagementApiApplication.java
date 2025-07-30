fb8afe7133bed27214c32ea1bd17df773bc43c85
김영훈 <hunifive@gmail.com>
2025-07-30 10:10:02 +0900

    first commit

---
+++ src/main/java/com/example/schedulemanagementapi/ScheduleManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManagementApiApplication.class, args);
+    }
+
+}
